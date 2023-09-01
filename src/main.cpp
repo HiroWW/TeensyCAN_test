e3c1960089be0775956f396223a02d71854dbc98
hiroaki <hiroaki.tokyu8500@gmail.com>
2023-09-01 16:26:41 +0900

    canpackを送信できるようにした　フリーズが確認された（絶望）

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,25 @@ FlexCAN_T4FD<CAN3, RX_SIZE_256, TX_SIZE_256> FD;
 
 TeensyCAN node100 = TeensyCAN(100);
 
+struct pack{}; //基底クラス
+struct MasterToTail: public pack{
+    float updateTime;
+    float drCommand;
+    float deCommand;
+    float err_state[15];
+    float gravity[3];
+    float mode;
+};
+
+// MasterToIF mip;
+// IFToMaster imp;
+MasterToTail mtp;
+
+// TailToMaster tmp;
+// TailToIF tip;
+
+int loopCount = 0;
+
 void canSniff(const CANFD_message_t &msg) {
   Serial.print("ISR - MB "); Serial.print(msg.mb);
   Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
@@ -45,6 +64,28 @@ void loop() {
   // FD.events();
   // Node.events();
   static uint32_t t = millis();
+
+  // setup Master to Tail pack and send it to CAN bus
+  mtp.updateTime = 11.1f * (loopCount % 5 + 1);
+  mtp.drCommand = 22.2f * (loopCount % 5 + 1);
+  mtp.deCommand = 1.0f * (loopCount % 5 + 1);
+  for (int i = 0; i < 15; i++){
+    mtp.err_state[i] = 1.0f * i;
+  }
+  mtp.gravity[0] = 33.4f;
+  mtp.gravity[1] = 66.8f;
+  mtp.gravity[2] = 99.4f;
+  mtp.mode = 2.0f * (loopCount % 5 + 1);
+
+  union send_data_union {
+    MasterToTail raw_data;
+    uint8_t encoded_data[256];
+  };
+  send_data_union sendDataUnion; //* unionのインスタンスを宣言
+  sendDataUnion.raw_data = mtp; //* 引数に取ったstructをunionへコピー
+  // Serial.println("send start");
+  // Serial.println(Node.sendMsg(sendDataUnion.encoded_data, 256, 7));
+
   if ( millis() - t > 1000 ) {
     uint8_t data[1024] = {
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -56,9 +97,11 @@ void loop() {
       61, 62, 63, 64, 65, 66, 67, 68, 69, 70
     };
     Serial.println("Send Start");
-    // Serial.println(    Node.sendMsg(data, 40, 5)); /* Global always returns 1, no response. */
-    Serial.println(node100.sendMsg(data, 100, 7)); /* ACK: 0x06, TIMEOUT = 0xFF */
+    // Serial.println(    Node.sendMsg(data, 256, 5)); /* Global always returns 1, no response. */
+    // Serial.println(node100.sendMsg(data, 100, 7)); /* ACK: 0x06, TIMEOUT = 0xFF */
+    Serial.println(Node.sendMsg(sendDataUnion.encoded_data, 256, 3));
     t = millis();
   }
+  loopCount++ ;
 }
 
